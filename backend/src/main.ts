4f52adc5bbb7a3789e74fb9d37981f643268e50d
VitorVieira <vitorhvvieira@gmail.com>
2025-11-19 21:10:29 -0300

    Ajustando porta onde vai rodar o backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   );
 
   const port = configService.get<number>('PORT') || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   
   console.log(`🚀 Server running on http://localhost:${port}`);
 }
